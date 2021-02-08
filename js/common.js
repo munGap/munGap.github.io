c9f4f3c3ab467d70d417a653634bb31b5da60779
gslee <gs.lee@sntsoft.co.kr>
2021-02-09 03:21:04 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -232,8 +232,8 @@ function kakaoMap(x, y) { // 다음 지도
 	var markerPosition  = new kakao.maps.LatLng(x, y); 
 	
 	var imageSrc = './images/page/marker.png', // 마커이미지의 주소입니다    
-    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
-    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    imageSize = new kakao.maps.Size(64, 64), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 75)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
 	// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
 	var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
